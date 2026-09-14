5be5bae4c8a1da4bb63082dad302c924680e15d0
Yoni <yonilev2021@gmail.com>
2026-09-14 11:27:45 +0300

    Initial Commit

---
+++ Producer/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Producer.Services;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        services.AddHttpClient();
+        services.AddSingleton<KafkaProducerService>(provider =>
+        new KafkaProducerService("localhost:9092"));
+
+        services.AddHostedService<StationInformationService>();
+        services.AddHostedService<StationStatusService>();
+        services.AddHostedService<VehicleTypesService>();
+    }).Build();
+
+await host.RunAsync();
---
+++ Producer/Services/KafkaProducerService.cs
@@ -0,0 +1,27 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Producer.Services;
+
+public class KafkaProducerService
+{
+    private readonly string _bootstrapServer;
+    private readonly IProducer<Null, string> _producer;
+
+    public KafkaProducerService(string bootstrapServer)
+    {
+        _bootstrapServer = bootstrapServer;
+
+        var config = new ProducerConfig
+        {
+            BootstrapServers = _bootstrapServer
+        };
+
+        _producer = new ProducerBuilder<Null, string>(config).Build();
+    }
+
+}
