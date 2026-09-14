c0a6cc34b0d93af4a8b2f65b03a96ee668b65acf
Yoni <yonilev2021@gmail.com>
2026-09-14 12:16:55 +0300

    completed station info service

--- Producer/Models/StationInformation.cs
+++ Producer/Models/StationInformation.cs
@@ -1,11 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
 
 namespace Producer.Models;
 
 public class StationInformation
 {
+    [JsonPropertyName("station_id")]
+    public string StationId { get; set; } = string.Empty;
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; } = string.Empty;
+
+    [JsonPropertyName("short_name")]
+    public string ShortName { get; set; } = string.Empty;
+
+    [JsonPropertyName("lon")]
+    public double Lon { get; set; }
+
+    [JsonPropertyName("lat")]
+    public double Lat { get; set; }
+
+    [JsonPropertyName("region_id")]
+    public string RegionId { get; set; } = string.Empty;
+
+    [JsonPropertyName("capacity")]
+    public int Capacity { get; set; }
+
+    [JsonPropertyName("rental_uris")]
+    public RentalUrisDto RentalUris { get; set; } = null!;
+}
+
+public class RentalUrisDto
+{
+    [JsonPropertyName("android")]
+    public string Android { get; set; } = string.Empty;
+
+    [JsonPropertyName("ios")]
+    public string Ios { get; set; } = string.Empty;
 }
--- Producer/Program.cs
+++ Producer/Program.cs
@@ -3,12 +3,19 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Producer.Services;
 
+IConfiguration configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false)
+    .Build();
+
+var bootstrapServer = configuration["Kafka:BootstrapServers"] ?? "localhost:9092";
+
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((context, services) =>
     {
         services.AddHttpClient();
         services.AddSingleton<KafkaProducerService>(provider =>
-        new KafkaProducerService("localhost:9092"));
+        new KafkaProducerService(bootstrapServer));
 
         services.AddHostedService<StationInformationService>();
         services.AddHostedService<StationStatusService>();
