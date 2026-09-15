0d53bf8e0013c6b5995244061648543fe322615d
Yoni <yonilev2021@gmail.com>
2026-09-15 11:49:43 +0300

    created dbcontext in consumer

---
+++ Consumer/Data/PiplineDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using O.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Consumer.Models;
+namespace Consumer.Data;
+
+public class PiplineDbContext : DbContext
+{
+    public PiplineDbContext(DbContextOptions<PiplineDbContext> options)
+        :base(options)
+    { }
+
+    public DbSet<StationInformation> StationInfo = null!;
+    public DbSet<VehicleType> VehicleTypes = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<StationInformation>().
+            HasKey(s => s.StationId);
+
+        modelBuilder.Entity<VehicleType>()
+            .HasKey(s => s.VehicleTypeId);
+    }
+}
